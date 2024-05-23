ea28e2eea1a1f0549e6b5ed44d5d3f825e95098e
Raven Alexander <theravenalexander@gmail.com>
2024-05-23 11:33:59 -0600

    models

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+import 'dotenv/config';
 
-const port = 3000
+import mongoConfig from './config.js';
+
+const app = express();
+
+const port = 3000;
 
 app.get('/', () => (req, res) => {
     res.json('Hello! (from Server)')
-})
+});
 
 app.listen(port, () => {
     console.log('Listening on port: ' + port)
-})
+    mongoConfig()
+});
